d1aadff54ce9d3b357a2ff0896bf38c6d4448758
Sergey Lozenkov <beelesnik@gmail.com>
2018-06-06 22:27:43 +0400

    newcap2

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:beelesnik/testcap.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/data/testcap"
+set :deploy_to, "/data/newcap2"
 
 # Default value for :format is :airbrussh.
 set :format, :pretty
